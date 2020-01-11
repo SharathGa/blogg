f9c04c017ecab7706013851992d9fc6046b34a20
SharathG <sharathga808@gmail.com>
2020-01-10 19:48:54 -0600

    Used Jinja Templating
    set the title for home and about pages, used templating using jinja .
    
    [Ticket: X]

--- web_blog/apps.py
+++ web_blog/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class WebBlogConfig(AppConfig):
-    name = 'web_blog'
+    name = "web_blog"
--- web_blog/views.py
+++ web_blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    # this dummy data is a list of dictionaries.
+    {
+        "author": "Sharath Gangalapadu",
+        "title": "First Blog post",
+        "content": "First Post Content",
+        "date-posted": "January 10, 2020",
+    },
+    {
+        "author": "Angelina",
+        "title": "Second Blog post",
+        "content": "Bhai this no fap is difficult to control ",
+        "date-posted": "January 10, 2020",
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, "web_blog/home.html")
+    context = {"posts": posts}
+    return render(request, "web_blog/home.html", context,)
 
 
 # views always need to return a httpresponse or an exception, so when we use
 
 
 def about(request):
-    return render(request, "web_blog/about.html")
+    return render(request, "web_blog/about.html", {"title": " About"})
 
